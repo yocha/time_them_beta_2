f4e957bcd713b4844f6e1254ee9b39eed47be097
Yong Cha <yocha@yahoo.com>
2017-09-15 16:30:15 -0700

    delete event

--- app/src/main/java/com/upwiththekite/timethembeta2/TimeEventActivity.kt
+++ app/src/main/java/com/upwiththekite/timethembeta2/TimeEventActivity.kt
@@ -3,6 +3,7 @@ package com.upwiththekite.timethembeta2
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import io.realm.Realm
 
 class TimeEventActivity : AppCompatActivity() {
@@ -21,7 +22,28 @@ class TimeEventActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_time_event, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        val eventId = intent.getStringExtra("event")
+        val realm = Realm.getDefaultInstance()
+        val result = realm.where(Event::class.java).equalTo("id", eventId).findFirst()
+
+        return when (item.itemId) {
+            R.id.deleteEventItem -> {
+                realm.beginTransaction()
+                result?.deleteFromRealm()
+                realm.commitTransaction()
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
